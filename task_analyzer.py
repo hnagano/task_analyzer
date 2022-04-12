ce65b4a32fbba8d9b977940e2cc755ac3aded319
nagano <hrs.nagano@gmail.com>
2022-04-12 22:43:41 +0900

    first commit

---
+++ task_analyzer.py
@@ -0,0 +1,137 @@
+# -*- coding: utf-8 -*-
+import matplotlib
+import matplotlib.pyplot as plt
+import csv
+
+task_dict = {
+        "task_1" : 0,
+        "task_2" : 1,
+        }
+
+
+class Event(object):
+    def __init__(self):
+        self.type = ''
+        self.id = -1
+        self.time = 0
+        pass
+
+    def __repr__(self):
+        return "<Event type=%s id=%d time=%f>" % (self.type, self.id, self.time)
+
+class RunInfo(object):
+    def __init__(self, id, startTime, endTime):
+        self.id = id
+        self.startTime = startTime
+        self.endTime = endTime
+
+    def __repr__(self):
+        return "<RunInfo id='%d' startTime='%f' endTime='%f'>" % (self.id, self.startTime, self.endTime)
+
+class TaskGraph(object):
+    def __init__(self):
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(111)
+        self.runinfo_list = []
+        pass
+
+    #def set_range(self, x_range):
+        #self.x_range = x_range
+        #pass
+
+    def add_run_info(self, runinfo_list):
+        self.runinfo_list = runinfo_list
+
+    def show(self):
+        y_max = 0
+        x_min = 0xFFFFFFFF
+        x_max = 0
+        for elem in self.runinfo_list:
+            x = elem.startTime
+            y = elem.id * 1
+            width = elem.endTime - elem.startTime
+            height = 1
+            rect = plt.Rectangle((x, y),width, height, fc="#000077")
+            self.ax.add_patch(rect)
+            if y + height > y_max:
+                y_max = y + height
+            if x < x_min:
+                x_min = x
+            if x > x_max:
+                x_max = x
+            
+        self.x_range = (x_min, x_max)
+        plt.xlim(self.x_range)
+        plt.ylim((0, y_max))
+        plt.gca().invert_yaxis()
+        #  text = plt.Text(-50, 0.5, 'Task 1')
+        #  plt.gca().add_artist(text)
+        #  plt.text(-50, 0.5, 'Task 1')
+        my_yticks = []
+        y_ticks_num = []
+        for item in task_dict.items():
+            index = item[1]
+            name = item[0]
+            my_yticks.insert(index, name)
+            y_ticks_num.insert(index, 0.5 + index * 1)
+        print(my_yticks)
+        print(y_ticks_num)
+        plt.yticks(y_ticks_num, my_yticks)
+        #  plt.tick_params(labelleft='off')
+        plt.show()
+        
+
+class SectionInfo(object):
+    def __init__(self, start_symbol, end_symbol):
+        self.start_symbol = start_symbol
+        self.end_symbol = end_symbol
+
+def generate_run_sequence(eventlist):
+    runinfo_list = []
+    section = SectionInfo('TS_Start', 'TS_End')
+    
+    #parse event list into run info
+    id = -1
+    for event in eventlist:
+        if event.type == section.start_symbol:
+            id = event.id
+            startTime = event.time
+        elif event.type == section.end_symbol and event.id == id:
+            endTime = event.time
+            #  print("append " + str(startTime) + ", " + str(endTime))
+            runinfo_list.append(RunInfo(id, startTime, endTime))
+
+
+    return runinfo_list
+
+def read_eventlist_csv(filename):
+
+    eventlist = []
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
+        # parse csv into event
+        for row in reader:
+            event = Event()
+            task_name = row[0]
+            event.id = task_dict[task_name]
+            event.type = row[1]
+            event.time = float(row[2])
+            eventlist.append(event)
+    return eventlist
+
+def main():
+    graph = TaskGraph()
+
+    eventlist = read_eventlist_csv('sample_log.csv')
+    print(eventlist)
+
+    runinfo_list = generate_run_sequence(eventlist)
+    print(runinfo_list)
+
+    graph.add_run_info(runinfo_list)
+    #graph.set_range((22648, 26607))
+    graph.show()
+
+if __name__ == '__main__':
+    main()
+
